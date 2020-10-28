9ed6862a5975c8d8d8d7996c3bf52ffaa1bf101f
Fanka Bacheva <f.r.vasileva@gmail.com>
2020-10-28 14:06:23 +0200

    init commit

---
+++ src/weather-info-list/weather-info-list.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import WeatherInfoTile from "../components/weather-info-tile/weather-info-tile";
+
+export const WeatherInfoList = () => {
+  const apiUrl =
+    "http://api.weatherapi.com/v1/forecast.json?key=d399b4c72a3e4a0ba5b102144202710&q=Sofia&days=10";
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setState(data);
+      });
+  }, []);
+
+  console.log("state", state);
+
+  return (
+    <div>
+      <p>list</p>{" "}
+      <div className="container">
+        <div className="row">
+          {state &&
+            state.forecast.forecastday.map((element) => (
+              <div className="col-md-4">
+                <WeatherInfoTile item={element} />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherInfoList;
